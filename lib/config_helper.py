39d44e8aeda67d1077e9ad608c9213f31d6d7b90
Jye Lee <jlee@cloudpassage.com>
2016-12-14 10:16:07 -0800

    remove comments

--- lib/config_helper.py
+++ lib/config_helper.py
@@ -3,21 +3,6 @@ import re
 
 
 class ConfigHelper(object):
-    """This class contains all application configuration variables.
-
-    All configuration variables in this class are derived from environment
-    variables.
-
-    Attributes:
-        halo_api_key (str): Halo API key, sometimes referred to as 'key id'
-        halo_api_secret_key (str): Halo API secret associated with halo_api_key
-        halo_api_hostname (str): Hostname for Halo API
-        halo_api_port (str): Halo API port
-        slack_api_token (str): Slack API token
-        slack_username (str): Donbot's user name (purely cosmetic)
-        slack_icon (str): Donbot's avatar
-
-    """
     def __init__(self):
         self.ua = ConfigHelper.get_ua_string()
         self.product_version = ConfigHelper.get_product_version()
